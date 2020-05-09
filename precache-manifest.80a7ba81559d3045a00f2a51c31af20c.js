ab80794570bc182ef4a51e421c041dc7a66c9784
IgorB <iblanusa@gmail.com>
2020-05-09 12:10:24 +0200

    Updates

---
+++ precache-manifest.80a7ba81559d3045a00f2a51c31af20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c9b6c82d94ad9bfef3beb6037c373c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fe43d094cfb92a1df0aa",
+    "url": "/Calculator/static/css/main.09df99a0.chunk.css"
+  },
+  {
+    "revision": "7a403c578c71f40de11a",
+    "url": "/Calculator/static/js/2.53d3a4f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.53d3a4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe43d094cfb92a1df0aa",
+    "url": "/Calculator/static/js/main.80c9b26d.chunk.js"
+  },
+  {
+    "revision": "038518c59f2b3726162a",
+    "url": "/Calculator/static/js/runtime-main.9951bf2c.js"
+  }
+]);
